d2959c5325d373b6301d3a2925173438ff447d6d
MaFan8 <maple_f@hotmail.com>
2024-02-11 14:02:34 -0500

    Started chat completion

---
+++ openai_test/controllers/openaiController.js
@@ -0,0 +1,21 @@
+const openai = require('../config/openaiConfig')
+
+const generateMeta = async (title) => {
+    
+    const description = await openai.createChatCompletion ({
+        model:  "gpt-3.5-turbo",
+        messages: [
+            {
+                role: 'user',
+                // backticks for outputing variables inside
+                content: `Come up with a description for YouTube cideo called ${title}`
+            }
+        ],
+        // how may tokens to spend on this question
+        max_tokens: 100
+    })
+
+    console.log(description.data.choices[0].message)
+}
+
+module.exports = {generateMeta}
